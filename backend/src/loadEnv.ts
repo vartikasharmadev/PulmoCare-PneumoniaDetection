67ee77a4817900ae1f57b569e56d0c9ef57e825c
Vartika Sharma <vartikasharma@Vartikas-MacBook-Air.local>
2026-04-03 15:33:40 +0530

    clean backend remove node artifacts

--- backend/src/loadEnv.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-import { config as loadEnv } from 'dotenv';
-
-loadEnv({ path: path.resolve(process.cwd(), '..', '.env') });
-loadEnv({ path: path.resolve(process.cwd(), '.env') });
